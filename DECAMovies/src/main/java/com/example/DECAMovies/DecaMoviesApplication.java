7e8940c271cd3b210c9d5de3e658a06ab560304b
Dave <dave.b11@hotmail.com>
2021-04-09 13:33:47 +0200

    Netflix Clone Project Set Up
    Set up of Spring Boot Application

---
+++ DECAMovies/src/main/java/com/example/DECAMovies/DecaMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.example.DECAMovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecaMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecaMoviesApplication.class, args);
+	}
+
+}
---
+++ DECAMovies/src/test/java/com/example/DECAMovies/DecaMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DECAMovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecaMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
